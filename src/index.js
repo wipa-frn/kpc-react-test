0852e939fcfdf312abfa192f5d5f845651ec7c72
Wipawadee MONKHUT <wipawadee.mo@ku.th>
2020-05-29 23:14:11 +0700

    initialize project folder

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/main.scss';
+import CrudApp from './views/CrudApp';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CrudApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/views/CrudApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CrudApp() {
+  return (
+    <div className="crud-app">
+      <h2>CRUD APP</h2>
+    </div>
+  );
+}
+
+export default CrudApp;
