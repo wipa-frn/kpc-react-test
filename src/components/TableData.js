f1ea22d0006526163c3bab038103032b594342e7
Wipawadee MONKHUT <wipawadee.mo@ku.th>
2020-06-01 16:13:06 +0700

    add ui show table data

---
+++ src/components/TableData.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import {Table,Form,Button,Pagination} from 'react-bootstrap'
+
+const TableData = (props) => {
+  return ( 
+    <div className="table-data">
+      <div className="d-flex justify-content-between my-2">
+        <div className="d-flex align-items-start" >
+          <Form.Check
+            id="select-all"
+            custom
+            inline
+            label="Select All"
+            type="checkbox"
+          />
+          <Button size="sm" variant="danger">DELETE</Button>
+        </div>
+        <div className="d-flex">
+          <Pagination size="sm">
+            <Pagination.Prev />
+            <Pagination.Item>{1}</Pagination.Item>
+            <Pagination.Item>{2}</Pagination.Item>
+            <Pagination.Item>{3}</Pagination.Item>
+            <Pagination.Item active>{4}</Pagination.Item>
+            <Pagination.Ellipsis />
+            <Pagination.Next />
+          </Pagination>
+        </div>
+      </div>
+
+      <Table responsive striped bordered hover variant="dark" >
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Nationality</th>
+            <th>MobilePhone</th>
+            <th></th>
+            {/* <th>Title</th> */}
+            {/* <th>Birthday</th> */}
+            {/* <th>CitizenID</th> */}
+            {/* <th>Passport</th> */}
+            {/* <th>Expected Salary</th> */}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>  
+              <Form.Check
+                id="order-i"
+                custom
+                inline
+                label=""
+                type="checkbox"
+              />
+            </td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+            <td className="text-right">
+              <a className="mr-1" href="#edit">EDIT</a>/
+              <a className="ml-1"href="#delete">DELETE</a>
+            </td>
+          
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+
+   );
+}
+ 
+export default TableData;
--- src/views/CrudApp.js
+++ src/views/CrudApp.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import PersonalForm from '../components/PersonalForm'
+import TableData from '../components/TableData'
 
 function CrudApp() {
   return (
-    <div className="crud-app d-flex flex-column justify-content-center align-items-center">
+    <div className="crud-app container d-flex flex-column justify-content-center">
       <h1 className="my-4">CRUD APP</h1>
       <PersonalForm/>
+      <div className="w-100 my-3">
+        <TableData/>
+      </div>
     </div>
   );
 }
