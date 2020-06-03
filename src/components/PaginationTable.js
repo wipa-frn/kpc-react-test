d2560c7b74c969526931b016dc5b5e6e52edffe7
Wipawadee MONKHUT <wipawadee.mo@ku.th>
2020-06-03 19:34:05 +0700

    add pagination

--- src/components/PaginationTable.js
+++ src/components/PaginationTable.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import {Pagination} from 'react-bootstrap'
 
-const PaginationTable = () => {
+const PaginationTable = (props) => {
+  const {dataPerPage, totalData, paginate, nextPage, prevPage, currentPage, showPage} = props
+  const pageNumbers = [];
+  const totalPage = Math.ceil(totalData / dataPerPage)
+
+  //when user define total of show page
+  if(showPage > 0){
+    for (let i = 1; ((i <= showPage) && (i <= totalPage)); i++) {
+      pageNumbers.push(i);
+    }
+  }else {
+    for (let i = 1; (i <= totalPage); i++) {
+      pageNumbers.push(i);
+    }
+  }
+
+  //when user click nextpage ---> show new page 
+  if(currentPage > pageNumbers[pageNumbers.length-1]){
+    pageNumbers.shift()
+    pageNumbers.push(currentPage)
+  }
+
   return ( 
     <Pagination size="sm">
-      <Pagination.Prev>PREV</Pagination.Prev>
-      <Pagination.Item>{1}</Pagination.Item>
-      <Pagination.Item>{2}</Pagination.Item>
-      <Pagination.Item>{3}</Pagination.Item>
-      <Pagination.Item active>{4}</Pagination.Item>
-      <Pagination.Ellipsis />
-      <Pagination.Next>NEXT</Pagination.Next>
-  </Pagination>
+      <Pagination.Prev onClick={() => prevPage()}>PREV</Pagination.Prev>
+      {
+        pageNumbers.map(num => (
+          <Pagination.Item 
+            key={num} 
+            active={currentPage === num ? true : false}
+            onClick={() => paginate(num)}>
+              {num}
+          </Pagination.Item>
+        ))
+      }
+      <Pagination.Ellipsis disabled/>
+      <Pagination.Next onClick={() => nextPage()}>NEXT</Pagination.Next>
+    </Pagination>
    );
 }
  
--- src/views/CrudApp.js
+++ src/views/CrudApp.js
@@ -4,12 +4,29 @@ import PersonalForm from '../components/PersonalForm'
 import TableData from '../components/TableData'
 import PaginationTable from '../components/PaginationTable'
 
-const Pagination = () => {
-  return <PaginationTable />
-}
 function CrudApp() {
   const personals = useSelector(state => state.personals) 
+  const [currentPage, setCurrentPage] = useState(1);
+  const dataPerPage = 10;
+  const indexOfLastPost = currentPage * dataPerPage;
+  const indexOfFirstPost = indexOfLastPost - dataPerPage;
+  const currentData = personals.slice(indexOfFirstPost, indexOfLastPost);
 
+  const paginate = pageNum => setCurrentPage(pageNum);
+  
+  const prevPage = () => {
+    //page must be start with 1
+    if(currentPage - 1 > 0){
+      setCurrentPage(currentPage - 1 )
+    }
+  }
+
+  const nextPage = () => {
+    //page must be end with total page
+    if (currentPage + 1 < Math.ceil(personals.length / dataPerPage)){
+      setCurrentPage(currentPage + 1 )
+    }
+  };
 
   return (
     <div className="crud-app container d-flex flex-column justify-content-center">
@@ -17,7 +34,23 @@ function CrudApp() {
       <PersonalForm/>
       <div className="w-100 my-3">
         {
-          personals.length > 0 ? <TableData currentPersonals={personals} Pagination={Pagination}/> : <h6 className="text-center">There is no data in the table.</h6>
+          personals.length > 0 ? 
+          <TableData 
+            currentPersonals={currentData} 
+            Pagination={() => {
+              return <PaginationTable 
+                dataPerPage={dataPerPage} 
+                totalData={personals.length} 
+                paginate={paginate} 
+                prevPage={prevPage}
+                nextPage={nextPage} 
+                currentPage={currentPage} 
+                showPage={4}
+              />
+              }
+            }
+          /> 
+          :<h6 className="text-center">There is no data in the table.</h6>
         }
       </div>
     </div>
